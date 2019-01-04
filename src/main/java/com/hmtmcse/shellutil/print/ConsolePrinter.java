541abb73032bda7881c39aaefc0acd4dabb407ac
Touhid Mia <hmtm.cse@gmail.com>
2019-01-04 14:13:13 +0600

    Shell Util

---
+++ src/main/java/com/hmtmcse/shellutil/print/ConsolePrinter.java
@@ -0,0 +1,22 @@
+package com.hmtmcse.shellutil.print;
+
+public class ConsolePrinter {
+
+    public static void printLine(Object object, String colorCode){
+        System.out.print(colorCode);
+        System.out.print(object);
+        System.out.println(ColorCode.RESET);
+    }
+
+    public static void errorPrint(Object object){
+        printLine(object, ColorCode.RED_BOLD.toString());
+    }
+
+    public static void debugPrint(Object object){
+        printLine(object, ColorCode.YELLOW_BOLD.toString());
+    }
+
+    public static void successPrint(Object object){
+        printLine(object, ColorCode.GREEN_BOLD.toString());
+    }
+}
--- src/main/java/com/hmtmcse/shellutil/util/ConsolePrinter.java
+++
@@ -1,4 +0,0 @@
-package com.hmtmcse.shellutil.util;
-
-public class ConsolePrinter {
-}
