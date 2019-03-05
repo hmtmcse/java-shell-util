cf27826d19f75f13dd718c93a8b1e4a07e3180cb
Touhid Mia <hmtm.cse@gmail.com>
2019-03-05 22:42:15 +0600

    Clean Old Method

--- src/main/java/com/hmtmcse/shellutil/console/menu/CLIMenuItemProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.hmtmcse.shellutil.console.menu;
-
-import com.hmtmcse.shellutil.common.ShellUtilException;
-
-import java.util.LinkedHashMap;
-
-public interface CLIMenuItemProcessor {
-    public void process(CLIOperatorInput inputs) throws ShellUtilException;
-}
